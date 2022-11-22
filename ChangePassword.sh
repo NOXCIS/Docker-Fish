c58670090840acdc3df42544f67ff73c27515fd1
NOXCIS <shamarakeillee@gmail.com>
2022-11-22 15:46:08 -0500

    Added script to change creds

---
+++ ChangePassword.sh
@@ -0,0 +1,13 @@
+cd app &&
+
+echo "#######################################################################"
+        echo "|LOGIN - CHANGE_PASSWORD| MAKE WANTED CHANGES SAVE AND EXIT"
+        echo "#######################################################################"
+        sleep 3s        main.py
+
+cd app/core &&
+
+ echo "#######################################################################"
+        echo "|APPSECRET - CHANGE_KEY| MAKE WANTED CHANGES SAVE AND EXIT"
+        echo "#######################################################################"     
+        sleep 3s        config.py
