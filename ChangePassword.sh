fc6000cc174482e55ba06df9cec812ab0cb44ba5
NOXCIS <shamarakeillee@gmail.com>
2022-11-22 16:13:22 -0500

    Update ChangePassword.sh

--- ChangePassword.sh
+++ ChangePassword.sh
@@ -6,10 +6,13 @@ echo "#######################################################################"
         sleep 3s       
         nano main.py
 
-cd app/core &&
+cd core &&
 
  echo "#######################################################################"
         echo "|APPSECRET - CHANGE_KEY| MAKE WANTED CHANGES SAVE AND EXIT"
         echo "#######################################################################"     
         sleep 3s       
         nano config.py
+
+cd .. &&
+cd ..
