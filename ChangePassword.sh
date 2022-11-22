6924a6269575157d0ab362b5b4649a6f401a04b2
NOXCIS <shamarakeillee@gmail.com>
2022-11-22 15:55:01 -0500

    Update ChangePassword.sh

--- ChangePassword.sh
+++ ChangePassword.sh
@@ -3,11 +3,13 @@ cd app &&
 echo "#######################################################################"
         echo "|LOGIN - CHANGE_PASSWORD| MAKE WANTED CHANGES SAVE AND EXIT"
         echo "#######################################################################"
-        sleep 3s        main.py
+        sleep 3s       
+        nano main.py
 
 cd app/core &&
 
  echo "#######################################################################"
         echo "|APPSECRET - CHANGE_KEY| MAKE WANTED CHANGES SAVE AND EXIT"
         echo "#######################################################################"     
-        sleep 3s        config.py
+        sleep 3s       
+        nano config.py
