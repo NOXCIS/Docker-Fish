2b650df07a4625a3ffc4034ef7d32487db24304b
NOXCIS <shamarakeillee@gmail.com>
2022-11-27 16:32:23 -0500

    Fixed Merge Break

--- flask/app/core/genToken.py
+++ flask/app/core/genToken.py
@@ -6,18 +6,13 @@ import os
 def genToken():
     return ''.join(secrets.token_urlsafe(16))
 
-print(genToken())
-
 def genQRCode(token, revoked=False):
-	qr = 'app/templates/static/token/qrcode.svg'
+	qr = 'templates/static/token/qrcode.svg'
 	if revoked:
 		os.remove(qr)
 	if not os.path.exists(qr):
 		factory = qrcode.image.svg.SvgImage
 		img = qrcode.make(token, image_factory=factory)
 		img.save(qr)
-		print(type(img))
-		
-
 	else:
 		os.remove(qr)
